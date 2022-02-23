0c083fa40f1f72de01731aba54440b9317981655
prakhargvp <prakhargvp@gmail.com>
2022-02-23 10:14:02 +0530

    Add sample products

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,45 @@
-import Head from 'next/head';
-import Image from 'next/image';
+import {
+  Button,
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Grid,
+  Typography,
+} from '@material-ui/core';
 import Layout from '../components/Layout';
-import styles from '../styles/Home.module.css';
+import data from '../utils/data';
 
 export default function Home() {
   return (
     <Layout>
       <div>
         <h1>Products</h1>
-        <ul>
-          <li>Product 1</li>
-          <li>Product 2</li>
-          <li>Product 3</li>
-        </ul>
+        <Grid container spacing={3}>
+          {data.products.map((product) => (
+            <Grid item md={4} key={product.name}>
+              <Card>
+                <CardActionArea component="a">
+                  <CardMedia
+                    component="img"
+                    image={product.image}
+                    title={product.name}
+                  ></CardMedia>
+                  <CardContent>
+                    <Typography> {product.name}</Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Typography>$ {product.price}</Typography>
+                    <Button size="small" color="primary">
+                      Add to cart
+                    </Button>
+                  </CardActions>
+                </CardActionArea>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
       </div>
     </Layout>
   );
--- utils/styles.js
+++ utils/styles.js
@@ -8,6 +8,13 @@ const useStyle = makeStyles({
       marginLeft: 10,
     },
   },
+  brand: {
+    fontWeight: 'bold',
+    fontSize: '1.5rem',
+  },
+  grow: {
+    flexGrow: 1,
+  },
   main: {
     minHeight: '80vh',
   },
