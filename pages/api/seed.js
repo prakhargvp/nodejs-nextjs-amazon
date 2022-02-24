72ae50df984f06cbb6a7e83da488546fa4dd0ef5
prakhargvp <prakhargvp@gmail.com>
2022-02-24 14:51:04 +0530

    seeding products to database

--- pages/api/seed.js
+++ pages/api/seed.js
@@ -1,6 +1,6 @@
 import nc from 'next-connect';
-import Product from '../../../models/Product';
-import db from '../../../utils/db';
+import Product from '../../models/Product';
+import db from '../../utils/db';
 import data from '../../utils/data';
 
 const handler = nc();
@@ -8,7 +8,7 @@ const handler = nc();
 handler.get(async (req, res) => {
   await db.connect();
   await Product.deleteMany();
-  await Product.inserMany(data.products);
+  await Product.insertMany(data.products);
   await db.disconnect();
   res.send({ message: 'seeded successfully' });
 });
