0fc36527ecfbf8b7747d386d9c90f8df872ec032
prakhargvp <prakhargvp@gmail.com>
2022-03-07 15:16:17 +0530

    List Orders for Admin

--- utils/auth.js
+++ utils/auth.js
@@ -32,4 +32,13 @@ const isAuth = async (req, res, next) => {
     res.status(401).send({ message: 'Token is not supplied' });
   }
 };
-export { signToken, isAuth };
+
+const isAdmin = async (req, res, next) => {
+  if (req.user.isAdmin) {
+    next();
+  } else {
+    res.status(401).send({ message: 'User is not admin' });
+  }
+};
+
+export { signToken, isAuth, isAdmin };
