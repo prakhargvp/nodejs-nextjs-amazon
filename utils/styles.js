374120bdfd877da407e5009ee7c75831608d65d0
prakhargvp <prakhargvp@gmail.com>
2022-03-09 09:10:18 +0530

    adding product rating

--- utils/styles.js
+++ utils/styles.js
@@ -44,6 +44,15 @@ const useStyle = makeStyles({
   fullWidth: {
     width: '100%',
   },
+  reviewForm: {
+    maxWidth: 800,
+    width: '100%',
+  },
+  reviewItem: {
+    marginRight: '1rem',
+    borderRight: '1px #808080 solid',
+    paddingRight: '1rem',
+  },
 });
 
 export default useStyle;
